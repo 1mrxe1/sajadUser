061e12d2e353e461f5e29ecc1cf438160dfd6421
FINAL AINZ <133069804+1mrxe1@users.noreply.github.com>
2024-07-31 19:18:44 +0300

    Update ahelp.py

--- phoenix/ahelp.py
+++ phoenix/ahelp.py
@@ -2,48 +2,48 @@ from telethon import events
 
 import phoenix.client
 client = phoenix.client.client
-@events.register(events.NewMessage(outgoing=True, pattern=".ahelp"))
+@events.register(events.NewMessage(outgoing=True, pattern=".رسوم"))
 async def ahelp(event):
 	client.parse_mode = "html"
 	await event.delete()
 	messagelocation = event.to_id
 	await event.client.send_message(messagelocation, ("""
-<b>ANIMATSIONS MENU</b>
+<b>قائمة الرسوم المتحركة</b>
 
-[01] Monster art - .monster
-[02] Pig art - .pig
-[03] Killer art - .killer
-[04] Gun art - .gun
-[05] Dog art - .dog
-[06] BIG hello - .hello
-[07] Hello my friend art - .hmf
-[08] Couple art - .couple
-[09] Superme art - .sup
-[10] Welcome text - .welc
-[11] Snake art - .asnake
-[12] Cat art - .cat
-[13] Bye text - .bye
-[14] Shitos art - .shitos
-[15] Dislike bigf - .dislike
-[16] HYPNO - .hypno
-[17] squ - .squ
-[18] Kiler - .kiler
-[19] Train anim - .train
-[20] Alien anim - .rocket
-[21] Heart anim - .hart
-[22] Raped anim - .raped
+[01] فن الوحش - .monster
+[02] فن الخنزير - .pig
+[03] فن القاتل - .killer
+[04] فن السلاح - .gun
+[05] فن الكلب - .dog
+[06] مرحباً كبيراً -.welc
+[07] مرحبا صديقي الفن - .hmf
+[08] فن الزوجين - .couple
+[09] فن السوبرمي - .sup
+[10] نص الترحيب - .welc
+[11] فن الثعبان - .asnake
+[12] فن القطط - .cat
+[13] نص الوداع - .bye
+[14] فن شيتوس - .shitos
+[15] لا أحب bigf - .dislike
+[16] التنويم المغناطيسي - .hypno
+[17] sq - .squ
+[18] كيلر - .kiler
+[19] قطار متحرك - .train
+[20] الرسوم المتحركة الغريبة - .rocket
+[21] قلب متحرك - .hart
+[22] حيوان مُغتصب - .raped
 [23] FNL - .fnl
-[24] Monkey anim - .monkey
-[25] Hands anim - .hands
-[26] Count number - .count
-[27] BIG F - .kf
-[28] F - .f {text}
-[29] BIG oof - .bigoff
-[30] Flower - .flower {text}
-[31] Hearts anim - .vheart {text}
-[32] Love you anim - .luvart {text}
-[33] I love you art - .iloveu
+[24] قرد متحرك - .monkey
+[25] الأيدي المتحركة - .hands
+[26] عدد الأرقام - .count
+[27] اففف كبير - .kf
+[28] اف اف -.f {نص}
+[29] عطلة كبيرة - .bigoff
+[30] زهرة - .flower {نص}
+[31] قلوب متحركة - .vheart {نص}
+[32] أحبك  أنمي - .luvart {نص}
+[33] أحبك  - .iloveu
 
-<b>Developer:</b> @programmer_www
-<b>Channel:</b> @phoenix_userbot
+
+<b>المطور:</b> @I0I0II 
 """))
